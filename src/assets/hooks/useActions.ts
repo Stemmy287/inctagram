f1b2e24a5cb9b77ff63d21d3e7df5f4b04bd4314
Pavlik1996 <miki2012miki@gmail.com>
2023-05-01 20:59:05 +0300

    add hooks

---
+++ src/assets/hooks/useActions.ts
@@ -0,0 +1,26 @@
+import { useMemo } from 'react'
+
+import { ActionCreatorsMapObject, bindActionCreators } from 'redux'
+
+import { useAppDispatch } from './useAppDispatch'
+
+export const useActions = <T extends ActionCreatorsMapObject>(actions: T) => {
+  const dispatch = useAppDispatch()
+
+  return useMemo(
+    () => bindActionCreators<T, RemapActionCreators<T>>(actions, dispatch),
+    [actions, dispatch]
+  )
+}
+
+// Types
+type IsValidArg<T> = T extends object ? (keyof T extends never ? false : true) : true
+type ActionCreatorResponse<T extends (...args: any[]) => any> = ReturnType<ReturnType<T>>
+type ReplaceReturnType<T, TNewReturn> = T extends (a: infer A) => infer R
+  ? IsValidArg<A> extends true
+    ? (a: A) => TNewReturn
+    : () => TNewReturn
+  : never
+type RemapActionCreators<T extends ActionCreatorsMapObject> = {
+  [K in keyof T]: ReplaceReturnType<T[K], ActionCreatorResponse<T[K]>>
+}
