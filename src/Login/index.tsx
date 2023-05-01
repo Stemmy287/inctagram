497cab0a576dbe139ace2d0e4d0772c28252e54a
ggavr <gimer.gavr@gmail.com>
2023-05-01 14:16:55 +0200

    commit login

---
+++ src/Login/index.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <>
+            <h2>Login...</h2>
+            <input type={'text'} placeholder={'login'}/>
+        </>
+    );
+};
+
+export default Login;
