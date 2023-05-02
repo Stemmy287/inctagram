4b1b9a781a63a97e659ca1365eeb88fea70f351e
Stemmy287 <steem287@gmail.com>
2023-05-02 18:51:38 +0300

    feat: notification

--- src/pages/forgotPassword/index.tsx
+++ src/pages/forgotPassword/index.tsx
@@ -10,22 +10,30 @@ import {TitlePopup} from "@/components/TitlePopup/TitlePopup";
 
 const ForgotPassword: NextPageWithLayout = () => {
   return (
-    <LoginDetailsWrapper>
-      <div className={s.container}>
-        <h1 className={s.title}>Forgot Password</h1>
-        <div>
-          <Input title="Email" value={''} onChange={() => {}}/>
-          <div className={s.desc}>
-            <span>Enter your email address and we will send you further instructions</span>
+    <>
+      <LoginDetailsWrapper>
+        <div className={s.container}>
+          <h1 className={s.title}>Forgot Password</h1>
+          <div>
+            <Input title="Email" value={''} onChange={() => {}}/>
+            <div className={s.desc}>
+              <span>Enter your email address and we will send you further instructions</span>
+            </div>
           </div>
+          <Button title="Send Link" callback={() => {}}/>
+          <Link className={s.link} href={''}>Back to Sign In</Link>
         </div>
-        <Button title="Send Link" callback={() => {}}/>
-        <Link className={s.link} href={''}>Back to Sign In</Link>
-      </div>
+      </LoginDetailsWrapper>
       <Popup onClose={() => {}}>
         <TitlePopup title="Email sent" onClose={() => {}}/>
+        <div className={s.notification}>
+          <span>We have sent a link to confirm your email to epam@epam.com</span>
+          <div className={s.btn}>
+            <Button title="OK" callback={() => {}}/>
+          </div>
+        </div>
       </Popup>
-    </LoginDetailsWrapper>
+    </>
   );
 };
 
