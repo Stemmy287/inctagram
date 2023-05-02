d227309374d5927ed7b86045e3499ab42b044530
Stemmy287 <steem287@gmail.com>
2023-05-02 15:05:30 +0300

    feat: flux

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Input.module.scss'
 
 type PropsType = {
   title?: string
   disabled?: boolean
+  value: string
+  onChange: (value: string) => void
 }
 
-export const Input = ({title, disabled}: PropsType) => {
+export const Input = ({title, disabled, value, onChange}: PropsType) => {
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.currentTarget.value)
+  }
+
  return (
    <div className={s.container}>
      {title && <span>{title}</span>}
-     <input disabled={disabled}/>
+     <input value={value} onChange={onChangeHandler} disabled={disabled}/>
    </div>
 
  )
