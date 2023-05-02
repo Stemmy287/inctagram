b581fbf91494c9f7f777f9ee62b6ffb21a544bcf
Stemmy287 <steem287@gmail.com>
2023-05-02 11:51:22 +0300

    feat: layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {NextPage} from "next";
+import {ReactElement, ReactNode} from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}> = NextPage<P> & {
+	getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+	const getLayout = Component.getLayout ?? ((page) => page)
+
+	return getLayout(<Component {...pageProps} />)
 }
