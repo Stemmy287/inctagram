a331ee7e2581a10a696e64eb11dcab1d2810e836
Stemmy287 <steem287@gmail.com>
2023-05-03 16:18:07 +0300

    feat: flow without api

---
+++ src/components/Captcha/Captcha.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
+import s from './Captcha.module.scss'
+
+type PropsType = {
+  callback: (value: string) => void
+  error: boolean
+}
+
+export const Captcha = ({callback, error}: PropsType) => {
+
+  const onChangeHandler = (value: string) => {
+    callback(value)
+  }
+
+ return (
+  <div className={error ? s.error : ''}>
+    <ReCAPTCHA
+      sitekey="6LdUtdklAAAAAIAhcAayU_lJI6vMPtMNe3jbLtK0"
+      onChange={onChangeHandler}
+      theme={"dark"}
+      hl={'en'}
+    />
+    {error && <span>Please verify that you are not a robot</span>}
+  </div>
+ );
+};
--- src/pages/forgotPassword/index.tsx
+++ src/pages/forgotPassword/index.tsx
@@ -8,40 +8,54 @@ import Link from "next/link";
 import {Popup} from "@/components/Popup/Popup";
 import {TitlePopup} from "@/components/TitlePopup/TitlePopup";
 import {useState} from "react";
-import ReCAPTCHA from "react-google-recaptcha";
+import {SubmitHandler, useForm} from "react-hook-form";
+
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {Captcha} from "@/components/Captcha/Captcha";
 
 const ForgotPassword: NextPageWithLayout = () => {
 
   const [isActive, setIsActive] = useState(false)
+
+  const schema = yup.object().shape({
+    email: yup.string().email('email should be correct').required('field required'),
+    captcha: yup.string().required('enter captcha')
+  })
+
+  const {register, handleSubmit, setValue, formState: {errors}} = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data)
+    setIsActive(true)
+  }
 
   const onClosePopupHandler = () => {
     setIsActive(false)
   }
 
   const onCaptcha = (value: string) => {
-    console.log(value)
+    //@ts-ignore
+    setValue('captcha', value)
   }
 
   return (
     <>
       <LoginDetailsWrapper>
-        <div className={s.container}>
+        <form className={s.container} onSubmit={handleSubmit(onSubmit)}>
           <h1 className={s.title}>Forgot Password</h1>
           <div>
-            <Input title="Email" value={''} onChange={() => {}}/>
+            <Input title="Email" register={register} name={'email'} error={errors.email?.message || ''}/>
             <div className={s.desc}>
               <span>Enter your email address and we will send you further instructions</span>
             </div>
           </div>
-          <Button title="Send Link" callback={() => {}}/>
+          <Button title="Send Link" callback={() => {}} disabled={!!errors.email}/>
           <Link className={s.link} href={''}>Back to Sign In</Link>
-          <ReCAPTCHA
-            sitekey="6LdUtdklAAAAAIAhcAayU_lJI6vMPtMNe3jbLtK0"
-            onChange={onCaptcha}
-            theme={"dark"}
-            hl={'en'}
-          />
-        </div>
+          <Captcha callback={onCaptcha} error={!!errors.captcha?.message}/>
+        </form>
       </LoginDetailsWrapper>
       {isActive &&
         <Popup onClose={onClosePopupHandler}>
@@ -49,7 +63,7 @@ const ForgotPassword: NextPageWithLayout = () => {
           <div className={s.notification}>
             <span>We have sent a link to confirm your email to epam@epam.com</span>
             <div className={s.btn}>
-             <Button title="OK" callback={onClosePopupHandler}/>
+              <Button title="OK" callback={onClosePopupHandler}/>
             </div>
           </div>
         </Popup>}
@@ -59,3 +73,9 @@ const ForgotPassword: NextPageWithLayout = () => {
 
 ForgotPassword.getLayout = getLayout
 export default ForgotPassword
+
+//types
+export type FormData = {
+  email: string
+  captcha: string
+}
