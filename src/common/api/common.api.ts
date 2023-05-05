dc519e7d6aecf03f993c8226b872a53fdf4087f9
Pavlik1996 <miki2012miki@gmail.com>
2023-05-05 18:43:02 +0300

    refactor api

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
 
 export const API_URL = 'https://inctagram-api-git-main-shuliakleonid.vercel.app/api/'
 
@@ -11,3 +13,63 @@ instance.interceptors.request.use((config) => {
 	config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
 	return config
 })
+
+
+
+export const api = createApi({
+	reducerPath: 'authApi',
+	baseQuery: fetchBaseQuery({ baseUrl: API_URL, prepareHeaders: (headers) => {
+
+			const token = localStorage.getItem('token');
+
+			if (token) {
+				// Добавляем токен в заголовок Authorization
+				headers.set('Authorization', `Bearer ${token}`);
+			}
+
+			return headers;
+		} }),
+	tagTypes: ['API'],
+
+	endpoints: builder => ({
+		login: builder.mutation<LoginUpdateResponseType, any>({
+			query: (arg: LoginParamsType) => ({
+				body: arg,
+				url: 'auth/login',
+				method: 'POST'
+			})
+		}),
+		logout: builder.mutation({
+			query: () => ({
+				url: 'auth/logout',
+				method: 'POST'
+			})
+		}),
+		me: builder.query<MeResponseType, any>({
+			query: () => ({
+				url: 'auth/me'
+			})
+		})
+	})
+})
+
+export const { login, logout, me } = api.endpoints
+
+type LoginParamsType = {
+	email: string
+	password: string
+}
+
+
+type LoginUpdateResponseType = {
+	accessToken: string
+}
+
+
+type MeResponseType = {
+	userId: number
+	userName: string
+	email: string
+}
+
+
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,10 +1,15 @@
 import { ThunkDispatch, configureStore } from "@reduxjs/toolkit";
 import { AnyAction, combineReducers } from "redux";
+import { api } from '@/common/api/common.api'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+	[api.reducerPath]: api.reducer
+})
 
 export const store = configureStore({
 	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(api.middleware),
 })
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
