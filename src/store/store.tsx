692a408846558d98d2373e5c4413dd697cebf310
Pavlik1996 <miki2012miki@gmail.com>
2023-05-05 18:43:02 +0300

    refactor api

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,13 +1,18 @@
-import {combineReducers, configureStore, ThunkDispatch} from "@reduxjs/toolkit";
-import {AnyAction} from "redux";
+import { ThunkDispatch, configureStore } from "@reduxjs/toolkit";
+import { AnyAction, combineReducers } from "redux";
+import { api } from '@/common/api/common.api'
 
-const rootReducers = combineReducers({
-
+const rootReducer = combineReducers({
+	[api.reducerPath]: api.reducer
 })
 
 export const store = configureStore({
-  reducer: rootReducers
+	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(api.middleware),
 })
 
-export type AppRootStateType = ReturnType<typeof rootReducers>
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+
