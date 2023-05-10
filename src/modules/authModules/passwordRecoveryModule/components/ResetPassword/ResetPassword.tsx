8a8f39067495f59a8683809c26db99964c090840
Stemmy287 <steem287@gmail.com>
2023-05-10 14:34:30 +0300

    feat: password input

--- src/modules/authModules/passwordRecoveryModule/components/ResetPassword/ResetPassword.tsx
+++ src/modules/authModules/passwordRecoveryModule/components/ResetPassword/ResetPassword.tsx
@@ -12,17 +12,18 @@ import { yupResolver } from '@hookform/resolvers/yup'
 type PropsType = {}
 
 export const ResetPassword = ({}: PropsType) => {
-
 	const router = useRouter()
 
 	const [resetPassword] = useResetPasswordMutation()
 
 	const schema = yup.object().shape({
-		newPassword: yup.string()
+		newPassword: yup
+			.string()
 			.min(6, 'Password must be at least 6 characters')
 			.max(20, 'Password must be at most 20 characters')
 			.required('field required'),
-		passwordConfirmation: yup.string()
+		passwordConfirmation: yup
+			.string()
 			.min(6, 'Password must be at least 6 characters')
 			.max(20, 'Password must be at most 20 characters')
 			.oneOf([yup.ref('newPassword')], 'The password must match the new password')
@@ -39,7 +40,7 @@ export const ResetPassword = ({}: PropsType) => {
 
 	const onSubmit: SubmitHandler<Omit<ResetPasswordType, 'recoveryCode'>> = async data => {
 		if (router.query.code) {
-			resetPassword({newPassword: data.newPassword, recoveryCode: router.query.code.toString()})
+			resetPassword({ newPassword: data.newPassword, recoveryCode: router.query.code.toString() })
 		}
 	}
 
@@ -47,13 +48,20 @@ export const ResetPassword = ({}: PropsType) => {
 		<LoginDetailsWrapper>
 			<form className={s.container} onSubmit={handleSubmit(onSubmit)}>
 				<h1 className={s.title}>Create New Password</h1>
-				<Input register={register} name='newPassword' title='New password' error={errors.newPassword?.message} />
+				<Input
+					register={register}
+					name='newPassword'
+					title='New password'
+					error={errors.newPassword?.message}
+					password
+				/>
 				<div>
 					<Input
 						register={register}
 						name='passwordConfirmation'
 						title='Password confirmation'
 						error={errors.passwordConfirmation?.message}
+						password
 					/>
 					<span className={s.warn}>Your password must be between 6 and 20 characters</span>
 				</div>
