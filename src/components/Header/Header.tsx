66ffb959663a743c50fec2c54120fa3dfab99f2b
Stemmy287 <steem287@gmail.com>
2023-05-11 18:02:23 +0300

    feat: loading line

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import s from './Header.module.scss'
 
 export const Header = () => {
-  return (
-    <header className={s.header}>
-      <h1 className={s.title}>Inctagram</h1>
-    </header>
-  );
-};
+	return (
+		<header className={s.header}>
+			<h1 className={s.title}>Inctagram</h1>
+		</header>
+	)
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,8 +4,6 @@ import { Header } from '@/components/Header/Header'
 import s from './Layout.module.scss'
 
 export const Layout: NextPage<PropsWithChildren> = ({ children }) => {
-
-
 	return (
 		<div className={s.container}>
 			<Header />
