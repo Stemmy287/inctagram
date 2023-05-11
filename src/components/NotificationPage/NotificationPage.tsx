6a6b432c429775bab4addf32bc846a850a3c3f3f
Stemmy287 <steem287@gmail.com>
2023-05-11 13:27:14 +0300

    feat: Notification page

---
+++ src/components/NotificationPage/NotificationPage.tsx
@@ -0,0 +1,23 @@
+import React, { PropsWithChildren } from 'react'
+import s from './NotificationPage.module.scss'
+import { NextPage } from 'next'
+import Image, { StaticImageData } from 'next/image'
+
+type PropsType = {
+	title: string
+	message: string
+	image: StaticImageData
+}
+
+export const NotificationPage:NextPage<PropsWithChildren & PropsType> = ({title, message, image, children}) => {
+ return (
+  <div className={s.container}>
+		<div className={s.content}>
+			<h1>{title}</h1>
+			<span>{message}</span>
+			{children}
+		</div>
+		<Image src={image} alt={'notification img'}/>
+  </div>
+ );
+};
--- src/pages/auth/resend-reset-password-link/index.tsx
+++ src/pages/auth/resend-reset-password-link/index.tsx
@@ -1,20 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NextPageWithLayout } from '@/pages/_app'
 import { getLayout } from '@/components/Layout/BaseLayout/BaseLayout'
-import { ResendLink } from '@/components/ResendLink/ResendLink'
 import { useRecoveryPasswordMutation } from '@/modules/authModules'
+import { NotificationPage } from '@/components/NotificationPage/NotificationPage'
+import timeImage from '../../../../public/images/rafiki.png'
+import { Button } from '@/components/Button/Button'
+import { Popup } from '@/components/Popup/Popup'
+import { Notification } from '@/components/Notification/Notification'
 
 const ResetPasswordPage: NextPageWithLayout = () => {
-	const [recoveryPassword, { isLoading }] = useRecoveryPasswordMutation()
+	const [recoveryPassword, { isLoading, isSuccess , reset}] = useRecoveryPasswordMutation()
 
-	const onResendHandler = () => {
-		const email = localStorage.getItem('email')
+	const [email, setEmail] = useState<null | string>()
+
+	const [isActive, setIsActive] = useState(false)
+
+	const onClosePopupHandler = () => {
+		setIsActive(false)
+	}
+	const onResendHandler = async () => {
 		if (email) {
-			recoveryPassword({ email })
+			await recoveryPassword({ email })
 		}
 	}
 
-	return <ResendLink title={'invalid'} buttonTitle={'Resend link'} callback={onResendHandler} isLoading={isLoading} />
+	if (isSuccess) {
+		setIsActive(true)
+		reset()
+	}
+
+	useEffect(() => {
+		setEmail(localStorage.getItem('email'))
+	}, [])
+
+	return (
+		<>
+			<NotificationPage
+				title='Email verification link invalid'
+				message={'Looks like the verification link has expired. Not to worry, we can send the link again'}
+				image={timeImage}
+			>
+				<Button title='Resend link' callback={onResendHandler} disabled={isLoading} />
+			</NotificationPage>
+			{isActive && (
+				<Popup onClose={onClosePopupHandler}>
+					<Notification
+						title={'Email sent'}
+						buttonTitle={'OK'}
+						message={`We have sent a link to confirm your email to ${email}`}
+						onClose={onClosePopupHandler}
+					/>
+				</Popup>
+			)}
+		</>
+	)
 }
 
 ResetPasswordPage.getLayout = getLayout
