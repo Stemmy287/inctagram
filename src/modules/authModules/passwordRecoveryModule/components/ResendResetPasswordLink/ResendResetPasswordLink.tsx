0d89f64d44e15e2d8ce8f3f181be055ca35e447f
Stemmy287 <steem287@gmail.com>
2023-05-11 13:40:13 +0300

    fix: decomposition resend password link

---
+++ src/modules/authModules/passwordRecoveryModule/components/ResendResetPasswordLink/ResendResetPasswordLink.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { NextPageWithLayout } from '@/pages/_app'
+import { useRecoveryPasswordMutation } from '@/modules/authModules'
+import { NotificationPage } from '@/components/NotificationPage/NotificationPage'
+import timeImage from '../../../../../../public/images/rafiki.png'
+import { Button } from '@/components/Button/Button'
+import { Popup } from '@/components/Popup/Popup'
+import { Notification } from '@/components/Notification/Notification'
+
+export const ResendResetPasswordLink: NextPageWithLayout = () => {
+	const [recoveryPassword, { isLoading, isSuccess , reset}] = useRecoveryPasswordMutation()
+
+	const [email, setEmail] = useState<null | string>()
+
+	const [isActive, setIsActive] = useState(false)
+
+	const onClosePopupHandler = () => {
+		setIsActive(false)
+	}
+	const onResendHandler = async () => {
+		if (email) {
+			await recoveryPassword({ email })
+		}
+	}
+
+	if (isSuccess) {
+		setIsActive(true)
+		reset()
+	}
+
+	useEffect(() => {
+		setEmail(localStorage.getItem('email'))
+	}, [])
+
+	return (
+		<>
+			<NotificationPage
+				title='Email verification link invalid'
+				message={'Looks like the verification link has expired. Not to worry, we can send the link again'}
+				image={timeImage}
+			>
+				<Button title='Resend link' callback={onResendHandler} disabled={isLoading} />
+			</NotificationPage>
+			{isActive && (
+				<Popup onClose={onClosePopupHandler}>
+					<Notification
+						title={'Email sent'}
+						buttonTitle={'OK'}
+						message={`We have sent a link to confirm your email to ${email}`}
+						onClose={onClosePopupHandler}
+					/>
+				</Popup>
+			)}
+		</>
+	)
+}
--- src/pages/auth/resend-reset-password-link/index.tsx
+++ src/pages/auth/resend-reset-password-link/index.tsx
@@ -1,60 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { NextPageWithLayout } from '@/pages/_app'
 import { getLayout } from '@/components/Layout/BaseLayout/BaseLayout'
-import { useRecoveryPasswordMutation } from '@/modules/authModules'
-import { NotificationPage } from '@/components/NotificationPage/NotificationPage'
-import timeImage from '../../../../public/images/rafiki.png'
-import { Button } from '@/components/Button/Button'
-import { Popup } from '@/components/Popup/Popup'
-import { Notification } from '@/components/Notification/Notification'
+import { ResendResetPasswordLink } from '@/modules/authModules'
 
-const ResetPasswordPage: NextPageWithLayout = () => {
-	const [recoveryPassword, { isLoading, isSuccess , reset}] = useRecoveryPasswordMutation()
+const ResendResetPasswordLinkPage: NextPageWithLayout = () => <ResendResetPasswordLink />
 
-	const [email, setEmail] = useState<null | string>()
-
-	const [isActive, setIsActive] = useState(false)
-
-	const onClosePopupHandler = () => {
-		setIsActive(false)
-	}
-	const onResendHandler = async () => {
-		if (email) {
-			await recoveryPassword({ email })
-		}
-	}
-
-	if (isSuccess) {
-		setIsActive(true)
-		reset()
-	}
-
-	useEffect(() => {
-		setEmail(localStorage.getItem('email'))
-	}, [])
-
-	return (
-		<>
-			<NotificationPage
-				title='Email verification link invalid'
-				message={'Looks like the verification link has expired. Not to worry, we can send the link again'}
-				image={timeImage}
-			>
-				<Button title='Resend link' callback={onResendHandler} disabled={isLoading} />
-			</NotificationPage>
-			{isActive && (
-				<Popup onClose={onClosePopupHandler}>
-					<Notification
-						title={'Email sent'}
-						buttonTitle={'OK'}
-						message={`We have sent a link to confirm your email to ${email}`}
-						onClose={onClosePopupHandler}
-					/>
-				</Popup>
-			)}
-		</>
-	)
-}
-
-ResetPasswordPage.getLayout = getLayout
-export default ResetPasswordPage
+ResendResetPasswordLinkPage.getLayout = getLayout
+export default ResendResetPasswordLinkPage
