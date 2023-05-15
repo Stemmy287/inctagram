b39bbef0a8b83232e82961dd529b3df1a8b3eeb0
Zhanet <zhanet_f@mail.ru>
2023-05-15 19:36:46 +0300

    feat:create MainLayout,InfoLayout, navbar, LogoutButton

---
+++ src/components/Button/LogoutButton/LogoutButton.tsx
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+import s from '@/components/Button/LogoutButton/LogoutButton.module.scss';
+import Image from 'next/image';
+import logoutIcon from '/public/icons/logout.svg';
+import {LogoutModal} from '@/modules/authModules';
+
+
+export const LogoutButton=()=>{
+    const [isActive, setIsActive] = useState(false)
+
+    return(
+        <>
+            {!isActive
+                ? <button className={s.btn} onClick={() => setIsActive(true)}>
+                    <Image className={s.icon} src={logoutIcon} alt={'logout'}/>
+                    <div className={s.title}>Log Out</div>
+                </button>
+                : <LogoutModal setIsActive={setIsActive}/>
+            }
+        </>
+
+    )
+}
