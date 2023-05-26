9c6059def4e9ffe0614f19fbf0781556e6f3c3b7
Pavlik1996 <miki2012miki@gmail.com>
2023-05-26 12:44:56 +0300

    add avatars

--- src/modules/profileModules/createProfile/ProfileSettings/EditAvatar.tsx
+++ src/modules/profileModules/createProfile/ProfileSettings/EditAvatar.tsx
@@ -4,38 +4,32 @@ import Image from 'next/image'
 import { Button } from '@/components/Button/Button'
 import { AddAvatar } from '@/modules/profileModules/uploadImage/AddAvatar'
 import { useGetUserQuery } from '@/modules/profileModules/createProfile/createProfileApi'
+import { useAppSelector } from '@/assets/hooks/useAppSelector'
 
 export const EditAvatar = () => {
-	const {data} = useGetUserQuery()
+	const { data } = useGetUserQuery()
 	const newAva = data?.avatars[1].url
 	console.log(newAva)
+
+	const avaNewNew = useAppSelector(state => state.uploadImage.avatar)
 
 	const [ava, setAva] = useState<any>(defaultAva)
 	const [openModal, setOpenModal] = useState(false)
 	const onClose = () => {
 		setOpenModal(!openModal)
 	}
-	// useEffect(()=> {
-	// 	if (data) {
-	// 		setAva(newAva)
-	// 	}
-	// },[newAva])
+
+	useEffect(() => {
+		setAva(avaNewNew)
+	}, [avaNewNew])
+
 	return (
 		<div>
-			<Image
-				src={ava}
-				alt="ava"
-				width='300'
-				height='300'
-			/>
+			<Image src={ava} alt='ava' width='300' height='300' />
 			{/*<Button title={'AAA'} callback={()=>setAva(newAva)}/>*/}
-			<label>
-
-			</label>
-			<Button
-				callback={onClose}
-				title='Add a profile photo'/>
-			{openModal && <AddAvatar onClose={onClose}/>}
+			<label></label>
+			<Button callback={onClose} title='Add a profile photo' />
+			{openModal && <AddAvatar onClose={onClose} />}
 		</div>
 	)
 }
--- src/modules/profileModules/uploadImage/uploadImageApi.ts
+++ src/modules/profileModules/uploadImage/uploadImageApi.ts
@@ -1,11 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { API_URL } from '@/modules/authModules'
+import { string } from 'yup'
+import { uploadImageActions } from './uploadImageReducer'
 
 export const uploadImageApi = createApi({
 	reducerPath: 'uploadImageApi',
 	baseQuery: fetchBaseQuery({
 		credentials: 'include',
-		baseUrl: API_URL, prepareHeaders: (headers) => {
+		baseUrl: API_URL,
+		prepareHeaders: headers => {
 			const token = localStorage.getItem('token')
 			if (token) {
 				headers.set('Authorization', `Bearer ${token}`)
@@ -13,10 +16,10 @@ export const uploadImageApi = createApi({
 			return headers
 		}
 	}),
-	endpoints: (build) => {
+	endpoints: build => {
 		return {
 			uploadImage: build.mutation<any, File>({
-				query: (file) => {
+				query: file => {
 					const formData = new FormData()
 					formData.append('file', file)
 
@@ -25,9 +28,22 @@ export const uploadImageApi = createApi({
 						url: 'users/profile/avatar',
 						body: formData
 					}
+				},
+				async onQueryStarted(_, { dispatch, queryFulfilled }) {
+					const res = await queryFulfilled
+					dispatch(uploadImageActions.setAva({ ava: res.data.avatars[0].url }))
 				}
 			})
 		}
 	}
 })
 export const { useUploadImageMutation } = uploadImageApi
+
+type avatars = Array<avatarsType>
+
+type avatarsType = {
+	url: string
+	width: number
+	height: number
+	fileSize: number
+}
