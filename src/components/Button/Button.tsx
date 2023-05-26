0d40c9583edacb9b6d840a080198040d80b3f952
Stemmy287 <steem287@gmail.com>
2023-05-26 14:35:00 +0300

    feat: eslint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from 'react'
 import s from './Button.module.scss'
 
 type PropsType = {
-  title: string
-  callback?: () => void
-  disabled?: boolean
+	title: string
+	callback?: () => void
+	disabled?: boolean
 }
 
-export const Button = ({title, callback, disabled}: PropsType) => {
-
- return <button className={s.btn} disabled={disabled} onClick={callback}>{title}</button>;
-
-};
+export const Button = ({ title, callback, disabled }: PropsType) => {
+	return (
+		<button className={s.btn} disabled={disabled} onClick={callback}>
+			{title}
+		</button>
+	)
+}
