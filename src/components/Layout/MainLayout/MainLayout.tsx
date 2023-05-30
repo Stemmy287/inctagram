67d13c8bd9a91d794cc870e42ffeba1da2fad838
Stemmy287 <steem287@gmail.com>
2023-05-30 17:15:26 +0300

    fix: main layout

--- src/components/Layout/MainLayout/MainLayout.tsx
+++ src/components/Layout/MainLayout/MainLayout.tsx
@@ -1,17 +1,35 @@
-import React, { PropsWithChildren, ReactElement } from 'react'
+import React, { PropsWithChildren, ReactElement, useEffect } from 'react'
 import { NextPage } from 'next'
 import s from '@/components/Layout/MainLayout/MainLayout.module.scss'
 import { Layout } from '@/components/Layout/Layout'
 import { Navbar } from '@/components/Navbar/Navbar'
+import { useAppSelector } from '@/assets/hooks/useAppSelector'
+import { loggedIn } from '@/modules/authModules'
+import { useRouter } from 'next/router'
+import { SnackBar } from '@/components/SnackBar/SnackBar'
 
 export const MainLayout: NextPage<PropsWithChildren> = ({ children }) => {
+	const isLoggedIn = useAppSelector(loggedIn)
+	const error = useAppSelector(state => state.app.error)
+
+	const router = useRouter()
+
+	useEffect(() => {
+		if (!isLoggedIn) {
+			router.push('/auth/login')
+		}
+	}, [isLoggedIn, router])
+
 	return (
-		<Layout>
-			<div className={s.main}>
-				<Navbar />
-				<div className={s.contentBox}>{children}</div>
-			</div>
-		</Layout>
+		<>
+			<Layout>
+				<div className={s.main}>
+					<Navbar />
+					<div className={s.contentBox}>{children}</div>
+				</div>
+			</Layout>
+			{error && <SnackBar />}
+		</>
 	)
 }
 
