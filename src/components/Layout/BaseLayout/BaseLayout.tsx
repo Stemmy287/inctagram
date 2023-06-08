4b3e7867f089ee44e6805fe3b38b4c5613c9f9f9
Pavlik1996 <miki2012miki@gmail.com>
2023-06-08 09:36:54 +0300

    refactor rtk qury, add flow update-tokens

--- src/components/Layout/BaseLayout/BaseLayout.tsx
+++ src/components/Layout/BaseLayout/BaseLayout.tsx
@@ -1,25 +1,17 @@
 import React, { PropsWithChildren, ReactElement, useEffect } from 'react'
 import { NextPage } from 'next'
 import { Preloader } from 'components/Preloader/Preloader'
-import { useMeQuery, useUpdateTokensMutation } from 'modules/authModules/authApi/authApi'
+import { useMeQuery } from 'modules/authModules/authApi/authApi'
 import { useAppSelector } from 'assets/hooks/useAppSelector'
 import { selectAppError, selectIsInitialized } from 'modules/appModules/appSelectors'
 import { Layout } from 'components/Layout/Layout'
 import { SnackBar } from 'components/SnackBar/SnackBar'
 
 export const BaseLayout: NextPage<PropsWithChildren> = ({ children }) => {
-	// const [updateTokens] = useUpdateTokensMutation()
 	const {} = useMeQuery()
 
 	const error = useAppSelector(selectAppError)
 	const isInitialized = useAppSelector(selectIsInitialized)
-	//
-	// useEffect(() => {
-	// 	// Refresh token every 55 minutes
-	// 	setInterval(() => {
-	// 		updateTokens()
-	// 	}, 55 * 60 * 1000)
-	// }, [updateTokens])
 
 	return (
 		<>
