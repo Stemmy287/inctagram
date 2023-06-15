7832125a229052f5e48b4027565a96e31601d192
stemmy287 <steem287@gmail.com>
2023-06-15 14:22:48 +0300

    fix: invalidate data profile

--- src/modules/profileModules/profileApi/profileApi.ts
+++ src/modules/profileModules/profileApi/profileApi.ts
@@ -5,6 +5,7 @@ import { profileActions } from '../profileReducer/profileReducer'
 export const profileApi = createApi({
 	reducerPath: 'profileApi',
 	baseQuery: baseQueryWithReauth,
+	tagTypes: ['Profile'],
 	endpoints: builder => ({
 		fetchProfile: builder.query<FetchUserResponseType, null>({
 			query: () => ({
@@ -14,27 +15,31 @@ export const profileApi = createApi({
 			async onQueryStarted(_, { dispatch, queryFulfilled }) {
 				const res = await queryFulfilled
 				dispatch(profileActions.setUser({ user: res.data }))
-			}
+			},
+			providesTags: ['Profile']
 		}),
 		createProfile: builder.mutation<FetchUserResponseType, ProfileType>({
 			query: body => ({
 				url: 'users/profile',
 				method: 'PUT',
 				body
-			})
+			}),
+			invalidatesTags: ['Profile']
 		}),
 		uploadImage: builder.mutation<UploadImageType, FormData | File>({
 			query: body => ({
 				url: 'users/profile/avatar',
 				method: 'POST',
 				body
-			})
+			}),
+			invalidatesTags: ['Profile'],
 		}),
 		deleteAvatar: builder.mutation<void, void>({
 			query: () => ({
 				url: 'users/profile/avatar',
 				method: 'DELETE'
-			})
+			}),
+			invalidatesTags: ['Profile']
 		})
 	})
 })
