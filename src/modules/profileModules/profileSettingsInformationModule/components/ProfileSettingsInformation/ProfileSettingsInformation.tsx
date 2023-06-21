9066085badd9c4a62e4c08c6efdf836b0b6eb50f
stemmy287 <steem287@gmail.com>
2023-06-21 13:49:55 +0300

    feat: dynamic invalidate cache

--- src/modules/profileModules/profileSettingsInformationModule/components/ProfileSettingsInformation/ProfileSettingsInformation.tsx
+++ src/modules/profileModules/profileSettingsInformationModule/components/ProfileSettingsInformation/ProfileSettingsInformation.tsx
@@ -4,8 +4,12 @@ import {
 import s from './ProfileSettingsInformation.module.scss'
 import { EditAvatar } from '../EditAva/EditAvatar'
 import { SettingsNavBar } from '../../../../../components/SettingsNavBar/SettingsNavBar'
+import { useFetchProfileQuery } from '../../../profileApi/profileApi'
 
 export const ProfileSettingsInformation = () => {
+
+	const {} = useFetchProfileQuery(null)
+
 	return (
 		<>
 			<SettingsNavBar/>
