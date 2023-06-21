f9867eb50e9dc24c7137fd1729505d47d70e4c26
stemmy287 <steem287@gmail.com>
2023-06-21 18:05:35 +0300

    fix: edit post refactor

--- src/modules/postModules/components/editPost/EditPost.tsx
+++ src/modules/postModules/components/editPost/EditPost.tsx
@@ -8,12 +8,10 @@ import Image from 'next/image'
 
 import { TitlePopup } from '../../../../components/TitlePopup/TitlePopup'
 import { Button } from '../../../../components/Button/Button'
-import * as yup from 'yup'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
 import { selectAppStatus } from '../../../appModules'
 import { selectShowedPost } from '../../postReducer/postReducer-selector'
 import { Avatar } from '../../../profileModules/profileSettingsInformationModule/components/Avatar/Avatar'
+import { TextArea } from '../../../../components/TextArea/TextArea'
 
 type PropsType = {
 	onClose: () => void
@@ -21,82 +19,62 @@ type PropsType = {
 	postId: string
 }
 
-type disType = {
-	description: string
-}
-
 export const EditPost = ({ onClose, description, postId }: PropsType) => {
-	const [isActive, setIsActive] = useState(false)
 	const [editPost] = useEditPostMutation()
 	const [value, setValue] = useState(description)
 	const user = useAppSelector(selectUser)
 	const appStatus = useAppSelector(selectAppStatus)
 	const postImage = useAppSelector(selectShowedPost)
 
-	const schema = yup.object().shape({
-		description: yup.string().required('add about')
-	})
-
-	const { register, handleSubmit } = useForm<disType>({
-		resolver: yupResolver(schema)
-	})
-
-	const onSubmit: SubmitHandler<disType> = data => {
-		editPost({ description: data.description, postId: postId })
+	const onEditPost = () => {
+		editPost({ description: value, postId: postId })
 	}
 
-	const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+	const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
 		setValue(e.currentTarget.value)
 	}
 	const onCloseHandler = () => {
-		setIsActive(false)
 	}
 
 	const onModalHandler = () => {
-		setIsActive(true)
 	}
 
 	return (
-		<>
-			<Popup onClose={onCloseHandler}>
-				<div className={s.editPost}>
-					<TitlePopup title={'Edit post'} onClose={onClose} />
-					<div className={s.wrapper}>
-						<div className={s.imageWrapper}>
-							<Image
-								className={s.closedPost}
-								src={postImage}
-								alt='post img'
-								width={485}
-								height={485}
-								onClick={onModalHandler}
+		<Popup onClose={onCloseHandler}>
+			<TitlePopup title={'Edit post'} onClose={onClose} />
+			<div className={s.wrapper}>
+				<Image
+					className={s.closedPost}
+					src={postImage}
+					alt='post img'
+					width={490}
+					height={490}
+					onClick={onModalHandler}
+				/>
+				<div className={s.content}>
+					<div className={s.userInfo}>
+						<Avatar small />
+						<span>{user?.userName}</span>
+					</div>
+					<div className={s.description}>
+						<div className={s.textArea}>
+							<TextArea
+								title='Add publication descriptions'
+								placeholder='Text'
+								value={value}
+								onChange={onChange}
+								maxLength={500}
 							/>
 						</div>
-						<div className={s.content}>
-							<div className={s.userInfo}>
-								<Avatar />
-								<span>{user?.userName}</span>
-							</div>
-							<div className={s.description}>
-								<form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-									<div className={s.descriptionBlock}>
-										<label>Add publication description</label>
-										<textarea
-											{...register('description')}
-											rows={4}
-											value={value}
-											onChange={onChangeHandler}
-										/>
-									</div>
-									<div className={s.btnBlock}>
-										<Button title='Save Changes' disabled={appStatus === 'loading'} />
-									</div>
-								</form>
-							</div>
-						</div>
+						{<span className={s.limit}>{value.length}/500</span>}
+						<Button
+							title='Save Changes'
+							disabled={appStatus === 'loading'}
+							callback={onEditPost}
+						/>
 					</div>
 				</div>
-			</Popup>
-		</>
+			</div>
+		</Popup>
 	)
 }
