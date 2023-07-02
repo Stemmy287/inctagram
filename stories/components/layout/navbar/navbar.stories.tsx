ddac63bf6b877aaf3391190af9c436af15f2b6ac
msa <serg.ks@gmail.com>
2023-07-03 00:03:08 +0400

    add: new navbar.stories.tsx

---
+++ stories/components/layout/navbar/navbar.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta } from '@storybook/react'
+import {Navbar} from '../../../../src/components/Navbar/Navbar';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from 'store/store';
+
+
+export default {
+  title: 'Components/Layout/navbar',
+  component: Navbar,
+  decorators: [(Story) => <Provider store={store}><Story /></Provider>],
+} as Meta<typeof Navbar>
+
+export const Primary = {
+  args: {}
+}
