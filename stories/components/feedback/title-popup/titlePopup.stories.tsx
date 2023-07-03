6b01d478c99bdf98f04e86dc37121d6748da7fb3
msa <serg.ks@gmail.com>
2023-07-03 18:53:39 +0400

    add: titlePopup.stories.tsx

---
+++ stories/components/feedback/title-popup/titlePopup.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@storybook/react'
+import {useState} from 'react';
+import {Button} from '../../../../src/components/Button/Button';
+import {TitlePopup} from '../../../../src/components/TitlePopup/TitlePopup';
+
+export default {
+  title: 'Components/Feedback/Title Popup',
+  component: TitlePopup,
+} as Meta<typeof TitlePopup>
+
+export const Default = {
+  render: (args: {title: string, onClose: () => void}) => {
+    const [open, setOpen] = useState(false)
+    return <>
+      {open ? <TitlePopup {...args} title={'Title popup!'} onClose={() => setOpen(!open)}/> : null}
+      {!open ? <Button callback={() => setOpen(true)} title={'Popup!'}/> : null}
+    </>
+  },
+}
