cd0f6170e4d7babfa220762a0b2b1083f06ad7c5
msa <serg.ks@gmail.com>
2023-07-03 15:27:55 +0400

    add: preloader.stories.tsx

---
+++ stories/components/feedback/preloader/preloader.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta } from '@storybook/react'
+import {Preloader} from '../../../../src/components/Preloader/Preloader';
+import {StoriesWrapper} from '../../../../storybook-utils/components/containers/vertical';
+
+
+export default {
+  title: 'Components/Feedback/Preloader',
+  component: Preloader,
+  decorators: [(Story) => <StoriesWrapper><Story/></StoriesWrapper>]
+} as Meta<typeof Preloader>
+
+export const Default = {
+}
