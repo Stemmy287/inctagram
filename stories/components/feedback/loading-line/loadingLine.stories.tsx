b10ebb945e8b819e290e53ca0c54b24b5cfce2c8
msa <serg.ks@gmail.com>
2023-07-03 14:23:15 +0400

    add: loadingLine.stories.tsx

---
+++ stories/components/feedback/loading-line/loadingLine.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react'
+import {LoadingLine} from '../../../../src/components/LoadingLine/LoadingLine';
+
+
+export default {
+  title: 'Components/Feedback/Loading Line',
+  component: LoadingLine,
+} as Meta<typeof LoadingLine>
+
+export const Default = {
+}
