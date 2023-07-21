8f56cf9536604a893bbc4cb1257ca4a0f34cab90
msa <serg.ks@gmail.com>
2023-07-22 01:45:06 +0400

    update: storybook 7.1

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,16 @@
-import type { StorybookConfig } from '@storybook/nextjs'
-
+import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
-	stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(ts|tsx)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-actions',
-		'@storybook/addon-backgrounds',
-	],
-	framework: {
-		name: '@storybook/nextjs',
-		options: {}
-	},
-	features: {
-		storyStoreV7: true
-	},
-
-	docs: {
-		autodocs: true
-	}
-}
-export default config
-
-
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-actions', '@storybook/addon-backgrounds', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  features: {
+    storyStoreV7: true
+  },
+  docs: {
+    autodocs: true
+  }
+};
+export default config;
