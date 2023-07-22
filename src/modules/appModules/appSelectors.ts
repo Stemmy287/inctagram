16e5a8d13dc44934feef8a1f39f1c247f8cf7b43
Stemmy287 <steem287@gmail.com>
2023-07-23 01:37:26 +0300

    check

--- src/modules/appModules/appSelectors.ts
+++ src/modules/appModules/appSelectors.ts
@@ -1,5 +1,5 @@
 import { AppRootStateType } from 'store/store'
 
-export const selectAppStatus = (state: AppRootStateType) => state.app.status
+export const selectAppStatus = (state: AppRootStateType) => state.app.status;
 export const selectAppError = (state: AppRootStateType) => state.app.error
 export const selectIsInitialized = (state: AppRootStateType) => state.app.isInitialized
